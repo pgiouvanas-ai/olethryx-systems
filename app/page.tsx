54b246c7de2a5c9060f94f78e6bd8096c0840f6f
Panagiotis Giouvanas <panagiotisgiouvanas@gmail.com>
2026-09-08 02:09:38 +0100

    feat: render footer on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return(
     <>
     <Header />
     <main className="pt-20">
-      
+
     </main>
+    <Footer />
     </>
   )
 }
