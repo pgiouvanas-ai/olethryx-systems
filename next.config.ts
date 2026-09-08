628d898d605ec0e1f1f63be10f0522dbcbc3871a
Panagiotis Giouvanas <panagiotisgiouvanas@gmail.com>
2026-09-08 22:27:59 +0100

    chore: disable next.js dev indicator
    Turned off the floating dev-mode badge: devIndicators: false, in next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* making the next logo disappear */
+  devIndicators: false,
 };
 
 export default nextConfig;
